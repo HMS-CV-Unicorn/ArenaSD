6e4bc4bdf31adf3ac8006425037f0b7174d62314
HMS-CV-Unicorn <swisscheese3777@gmail.com>
2025-12-21 13:41:43 +0900

    initial commit

---
+++ src/main/java/com/saratoga/arenaSD/ArenaSD.java
@@ -0,0 +1,17 @@
+package com.saratoga.arenaSD;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ArenaSD extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
